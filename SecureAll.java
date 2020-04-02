4fa070a0cb1899d3c4d9c9266ffe5453e9bbe31e
danir1512 <61586954+danir1512@users.noreply.github.com>
2020-04-02 17:49:57 +0100

    Add files via upload

---
+++ SecureAll.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package secureall;
+/**
+ *
+ * @author João
+ */
+public class SecureAll {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        UserInteraction userInteraction = new UserInteraction();
+        userInteraction.setSize(420, 325);
+        userInteraction.setVisible(true);
+    }
+    
+}
