ba88aa0ab4ae52e8e9ab6b5e25853901c2783ec9
danir1512 <61586954+danir1512@users.noreply.github.com>
2020-04-15 18:52:12 +0100

    Add files via upload

--- SecureAll.java
+++ SecureAll.java
@@ -4,20 +4,29 @@
  * and open the template in the editor.
  */
 package secureall;
+import java.sql.*;
+
 /**
  *
  * @author João
  */
+
 public class SecureAll {
     /**
      * @param args the command line arguments
+     * @throws java.lang.ClassNotFoundException
+     * @throws java.sql.SQLException
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        UserInteraction userInteraction = new UserInteraction();
+    public static void main(String[] args) throws ClassNotFoundException, SQLException    {
+        Class.forName("oracle.jdbc.driver.OracleDriver");
+        String url = "jdbc:oracle:thin:@localhost:32118:xe";
+        //String url = "jdbc:oracle:thin:@localhost:32118:xe";
+        Connection conn = DriverManager.getConnection(url,"mde","mde");
+        //conn.setAutoCommit(false);
+
+        UserInteraction userInteraction = new UserInteraction(conn);
         userInteraction.setSize(420, 325);
-        userInteraction.setVisible(true);
-        System.out.print("Buenos dias soy dani");
+        userInteraction.setVisible(true);    
+        
     }
-    
 }
